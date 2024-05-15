390ce17b0f42fe2451ac08317998e533881e94c4
Agatha-Moreira <1agatha.m.f@gmail.com>
2024-05-15 17:28:56 -0300

    feat: Finalizamos a parte JavaScrip dos cases e iniciamos a criação da API do site

--- script.js
+++ script.js
@@ -80,3 +80,48 @@ const mostrarSlideAnterior =  () => {
     // Renderiza o slideAtual
     banner.classList.add(slide[slideAtual])
 }
+
+const selecionarSlide = (indiceSlide) => {
+    slide.forEach(slide => banner.classList.remove(slide))
+
+    slideAtual = indiceSlide
+
+    banner.classList.add(slide[indiceSlide])
+}
+
+let listaCases = [
+    {
+        imagem: "https://unsplash.it/600/400?image=41",
+        descricao: "Uma empresa de tecnologia lança um desafio de gamificação ondes os funcionarios devem proopr e implementR IDEIAS INOVADORES"
+    },
+
+    {
+        imagem: "https://unsplash.it/600/400?image36",
+        descricao: "Uma empresa de consultiria cria uma narrativa interativa de gamificação para seu programa de trinamento"
+    },
+
+    {
+        imagem: "https://unsplash.it/600/400?27",
+        descricao: "Uma empresa de vendas imoplementa uma competição gamificada entre qiepes que competem pelo topo do ranking"
+    },
+
+    {
+        imagem: "https://unsplash.it/600/400?34",
+        descricao: "Uma empresa de saude promove o bem-estar dos funcionarios atravez de um desafio de gamificação de condicionamento fisico"
+    }
+]
+const renderizarCases = () => {
+    let elementoLista = document.getElementById("lista-cards")
+
+    // Templete strings
+    let template = ""
+
+    listaCases.forEach(cardCase => {
+        template += `<div class="card">
+        <img src="${cardCase.imagem}" alt="">
+        <p>${cardCase.descricao}</p>
+        <button>Ver mais</button></div>`
+    })
+
+    elementoLista.innerHTML = template
+}
